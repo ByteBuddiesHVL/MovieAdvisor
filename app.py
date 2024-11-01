13b035d7fa29dbd236354cebcb8abd5b951f0516
Dobzicle <669810@stud.hvl.no>
2024-11-01 19:14:53 +0100

    added html with tailwindcss

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pickle
 
 app = Flask(__name__)
 
-
+with open("model_31-10-24.pkl", "rb") as file:
+    model = pickle.load(file)
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def recommend():
+    if request.method == "POST":
+        genre_list = request.form['genres']
+        if genre_list:
+            prediction = model.predict([genre_list])
 
 if __name__ == '__main__':
     app.run()
